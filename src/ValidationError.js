9e40ecd367f825965b7f81d73b0800a74b7cd833
HamzaM3 <99541093+HamzaM3@users.noreply.github.com>
2022-11-26 17:05:18 +0100

    validation error is done

--- src/ValidationError.js
+++ src/ValidationError.js
@@ -1,5 +1,30 @@
+import { stringify } from "./stringify";
+
 export class ValidationError extends Error {
-  constructor (message) {
+  constructor (errorData) {
+    const { errorType, path, originalValue} = errorData;
+    let message;
+    switch (errorType) {
+      case 'WRONG_TYPE':
+        const { type, value } = errorData;
+        message = `Input has been invalidated:${ path ? ` (path: ${ path })` : '' } (indicator: ${type}) (typeof value: ${ typeof value }) (value: ${ stringify(value) })${originalValue ? ` (original value: ${ stringify(originalValue) })` : ''}`;
+        break;
+      case 'WRONG_NUMBER_FIELD':
+        const { expected, received } = errorData;
+        message = `Incorrect amount of object fields:${ path ? ` (path: ${ path })` : '' } (expected fields: ${JSON.stringify(expected)}) (received fields: ${JSON.stringify(received)})${originalValue ? ` (original value: ${ stringify(originalValue) })` : ''}`;
+        break;
+      case 'FIELD_IS_NOT_AN_INDICATOR':
+        message = `${path} is not a correct indicator`
+        break;
+      case 'NOT_JSONABLE':
+        message = `Input is not JSONABLE${ path ? `: (path: ${path})` : ''}`;
+        break;
+      default:
+        message = `indicatrice didn't not trigger this error ! Open an issue please !`
+        break;
+    }
     super(message)
+
+    this.errorData = errorData;
   }
 }
---
+++ tests/ValidationError.test.js
@@ -0,0 +1,60 @@
+import { ValidationError } from '../src/ValidationError';
+
+describe('ValidationError', () => {
+  it('is defined', () => {
+    expect(typeof ValidationError).toBe('function');
+    expect(() => new ValidationError({})).not.toThrow()
+  });
+
+  it('throws correct WRONG_TYPE errors without path nor original value', () => {
+    let error = new ValidationError({ errorType: 'WRONG_TYPE', type: 'boolean', value: 3 })
+    expect(error.message).toBe('Input has been invalidated: (indicator: boolean) (typeof value: number) (value: 3)')
+
+
+    error = new ValidationError({ errorType: 'WRONG_TYPE', type: 'non-array object', value: [3,4] })
+    expect(error.message).toBe('Input has been invalidated: (indicator: non-array object) (typeof value: object) (value: [3,4])')
+
+
+    error = new ValidationError({ errorType: 'WRONG_TYPE', type: 'number', value: {a:4,b:"str"} })
+    expect(error.message).toBe('Input has been invalidated: (indicator: number) (typeof value: object) (value: {"a":4,"b":"str"})')
+  });
+
+  it('throws correct WRONG_TYPE errors with path and original value', () => {
+    let error = new ValidationError({ errorType: 'WRONG_TYPE', type: 'number', value: "str", path: '.b.c', originalValue: {a:3,b:{c:"str"}} })
+    expect(error.message).toBe('Input has been invalidated: (path: .b.c) (indicator: number) (typeof value: string) (value: "str") (original value: {"a":3,"b":{"c":"str"}})')
+
+
+    error = new ValidationError({ errorType: 'WRONG_TYPE', type: 'non-array object', value: [3], path: '.a[1]', originalValue: {a: [{b:3},[3]]} })
+    expect(error.message).toBe('Input has been invalidated: (path: .a[1]) (indicator: non-array object) (typeof value: object) (value: [3]) (original value: {"a":[{"b":3},[3]]})')
+  });
+
+  it('throws correct WRONG_NUMBER_FIELD errors without path nor original value', () => {
+    let error = new ValidationError({ errorType: 'WRONG_NUMBER_FIELD', expected: ['a', 'b', 'c'], received: ['nope']})
+    expect(error.message).toBe('Incorrect amount of object fields: (expected fields: ["a","b","c"]) (received fields: ["nope"])')
+  });
+
+  it('throws correct WRONG_NUMBER_FIELD errors with path and original value', () => {
+    let error = new ValidationError({ errorType: 'WRONG_NUMBER_FIELD', path: '.data', expected: ['a', 'b', 'c'], received: ['nope'], originalValue: {data: {nope: "nope"}}})
+    expect(error.message).toBe('Incorrect amount of object fields: (path: .data) (expected fields: ["a","b","c"]) (received fields: ["nope"]) (original value: {"data":{"nope":"nope"}})')
+  });
+
+  it('throws correct FIELD_IS_NOT_AN_INDICATOR errors', () => {
+    let error = new ValidationError({ errorType: 'FIELD_IS_NOT_AN_INDICATOR', path: '.a[0]' })
+    expect(error.message).toBe('.a[0] is not a correct indicator')
+  });
+
+  it('throws correct NOT_JSONABLE errors without path', () => {
+    let error = new ValidationError({ errorType: 'NOT_JSONABLE' })
+    expect(error.message).toBe('Input is not JSONABLE')
+  });
+
+  it('throws correct NOT_JSONABLE errors with path', () => {
+    let error = new ValidationError({ errorType: 'NOT_JSONABLE', path: '.data'})
+    expect(error.message).toBe('Input is not JSONABLE: (path: .data)')
+  });
+
+  it('throws correct error for unknown causes', () => {
+    let error = new ValidationError({})
+    expect(error.message).toBe('indicatrice didn\'t not trigger this error ! Open an issue please !')
+  })
+});
